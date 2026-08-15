1ebf3ebae2cfe748660b35f5aaaff5046ea15d35
andstyle <andermuchael@gmail.com>
2026-08-15 20:02:24 -0300

    Corrigir cache do PWA para atualizar o index.html

--- sw.js
+++ sw.js
@@ -1,3 +1,13 @@
-const CACHE="delivery-comandas-v2";
-self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(["./","./index.html","./logo-moto.svg","./manifest.json"]))));
-self.addEventListener("fetch",e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
+const CACHE="delivery-comandas-v3";
+const ASSETS=["./","./index.html","./logo-moto.svg","./manifest.json"];
+self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
+self.addEventListener("activate",e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
+self.addEventListener("fetch",e=>{
+  if(e.request.method!=="GET") return;
+  const url=new URL(e.request.url);
+  if(e.request.mode==="navigate" || url.pathname.endsWith("/index.html")){
+    e.respondWith(fetch(e.request).then(r=>{const copy=r.clone();caches.open(CACHE).then(c=>c.put(e.request,copy));return r}).catch(()=>caches.match(e.request)));
+  }else{
+    e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)));
+  }
+});
