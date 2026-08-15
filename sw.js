5a83b55a90bfd65181db661a6cc196ad9918345b
andstyle <andermuchael@gmail.com>
2026-08-15 10:52:41 -0300

    Cache migration script with PWA assets

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE="delivery-comandas-v3";
-const ASSETS=["./","./index.html","./app.js","./logo-moto.svg","./manifest.json"];
+const CACHE="delivery-comandas-v4";
+const ASSETS=["./","./index.html","./auth-migrate.js","./app.js","./logo-moto.svg","./manifest.json"];
 self.addEventListener("install",e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)))});
 self.addEventListener("activate",e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});
 self.addEventListener("fetch",e=>{if(e.request.method!=="GET")return;e.respondWith(fetch(e.request).then(r=>{const copy=r.clone();caches.open(CACHE).then(c=>c.put(e.request,copy)).catch(()=>{});return r}).catch(()=>caches.match(e.request).then(c=>c||caches.match("./index.html"))) )});
