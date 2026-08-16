384e5f5ebfc25f0b5edde737ee1fcd230954ff41
andstyle <andermuchael@gmail.com>
2026-08-16 13:12:50 -0300

    Atualizar cache para nova aba de radio

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE="delivery-comandas-v10";
+const CACHE="delivery-comandas-v11";
 const ASSETS=["./","./index.html","./logo-moto.svg","./manifest.json","./ui-fixes.js"];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
