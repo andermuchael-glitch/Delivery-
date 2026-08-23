4ba3e33277ebacafd3e68be4e4dc23f51ceed4d2
andstyle <andermuchael@gmail.com>
2026-08-23 18:39:08 -0300

    Atualiza cache para novo visual da tela de login

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "entrega365-logo-v4";
+const CACHE = "entrega365-logo-v5-login";
 const AUTH = "./auth-fix.js";
 const ASSETS = ["./", "./index.html", "./manifest.json", "./logo-entrega365.png", "./logo-moto.svg", AUTH];
 self.addEventListener("install", event => {
