642146c6d4391f6e70d679a6fd7cbc4e3ed0382f
andstyle <andermuchael@gmail.com>
2026-08-23 12:28:50 -0300

    Force clients to load current Delivery365 after cache migration

--- sw.js
+++ sw.js
@@ -1,7 +1,13 @@
-const CACHE="entrega365-v42";
-const ASSETS=["./","./index.html?v=42","./logo-moto.svg?v=42","./manifest.json?v=42"];
+const CACHE="entrega365-v43";
+const ASSETS=["./","./index.html?v=43","./logo-moto.svg?v=43","./manifest.json?v=43"];
 self.addEventListener("install",event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)).then(()=>self.skipWaiting())));
-self.addEventListener("activate",event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key)))).then(()=>self.clients.claim())));
+self.addEventListener("activate",event=>event.waitUntil(
+  caches.keys()
+    .then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key))))
+    .then(()=>self.clients.claim())
+    .then(()=>self.clients.matchAll({type:"window",includeUncontrolled:true}))
+    .then(clients=>Promise.all(clients.map(client=>client.navigate(client.url))))
+));
 self.addEventListener("fetch",event=>{
   if(event.request.method!=="GET") return;
   const url=new URL(event.request.url);
