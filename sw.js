2a3e59b396ce33c15082f2b1b4126004b1062747
andstyle <andermuchael@gmail.com>
2026-08-24 21:17:43 -0300

    Atualiza cache do PWA para todas as novas funcionalidades

--- sw.js
+++ sw.js
@@ -1,9 +1,6 @@
-const CACHE="entrega365-pwa-v79";
-const AUTH="./auth-fix.js?v=79";
-const BRAND="./brand-update.js?v=79";
-const FEATURES="./entrega365-features.js?v=79";
-const DRIVE="./drive-backup.js?v=79";
-const ASSETS=["./","./index.html?v=79","./manifest.json?v=79","./app-icon.svg?v=79","./icon-72.svg?v=79","./logo-entrega365.jpg?v=79",AUTH,BRAND,FEATURES,DRIVE];
+const CACHE="entrega365-pwa-v80";
+const AUTH="./auth-fix.js?v=80",BRAND="./brand-update.js?v=80",FEATURES="./entrega365-features.js?v=80",DRIVE="./drive-backup.js?v=80",SESSION="./session-policy.js?v=80";
+const ASSETS=["./","./index.html?v=80","./manifest.json?v=80","./app-icon.svg?v=80","./icon-72.svg?v=80","./logo-entrega365.jpg?v=80",AUTH,BRAND,FEATURES,DRIVE,SESSION];
 self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener("activate",e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
 async function injectAuth(response){const type=response.headers.get("content-type")||"";if(!type.includes("text/html"))return response;let html=await response.text();if(html.includes("auth-fix.js"))return new Response(html,{status:response.status,statusText:response.statusText,headers:response.headers});const injected=html.replace(/<\/body>/i,`<script type="module" src="${AUTH}"></script><script src="${BRAND}"></script></body>`);const headers=new Headers(response.headers);headers.delete("content-length");return new Response(injected,{status:response.status,statusText:response.statusText,headers})}
