dcdf8dd401f1da46e81ed6be5309de2f996fad51
andstyle <andermuchael@gmail.com>
2026-08-30 09:17:09 -0300

    Adiciona configuração Capacitor isolada

---
+++ capacitor.config.ts
@@ -0,0 +1,15 @@
+const config = {
+  appId: 'com.entrega365.app.test',
+  appName: 'Entrega365 Teste',
+  webDir: 'android-test-web',
+  server: {
+    url: 'https://www.entrega365.com.br',
+    cleartext: false,
+    androidScheme: 'https'
+  },
+  android: {
+    allowMixedContent: false
+  }
+};
+
+export default config;
