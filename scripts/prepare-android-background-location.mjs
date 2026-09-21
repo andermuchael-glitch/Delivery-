629a9f80cafb431c0218626797b29ec907cf58a3
andstyle <andermuchael@gmail.com>
2026-09-21 14:46:17 -0300

    Corrigir import do PluginMethod no módulo de localização

--- scripts/prepare-android-background-location.mjs
+++ scripts/prepare-android-background-location.mjs
@@ -136,7 +136,7 @@ import com.getcapacitor.PluginCall;
 import com.getcapacitor.annotation.ActivityCallback;
 import com.getcapacitor.annotation.CapacitorPlugin;
 import com.getcapacitor.annotation.Permission;
-import com.getcapacitor.annotation.PluginMethod;
+import com.getcapacitor.PluginMethod;
 
 @CapacitorPlugin(
     name = "BackgroundLocation",
